97ce4dc4f8f0a394d837082407cc467c70f80788
Tara Dia <taracdia@gmail.com>
2023-01-30 07:34:45 -0600

    Removing unnecessary code

--- src/util/user.js
+++ src/util/user.js
@@ -11,28 +11,3 @@ export const userInfo = fetchedUserInfo.response.user;
  * {object[]} userBlogs - An array of blog objects the current user has post access to
  */
 export const userBlogs = userInfo?.blogs ?? [];
-
-/**
- * {string[]} userBlogNames - An array of blog names the current user has post access to
- */
-export const userBlogNames = userBlogs.map(blog => blog.name);
-
-/**
- * {object?} primaryBlog - The primary ("main") blog for the user
- */
-export const primaryBlog = userBlogs.find(blog => blog.primary === true);
-
-/**
- * {string?} primaryBlogName - The name of the user's primary blog
- */
-export const primaryBlogName = primaryBlog?.name;
-
-/**
- * {object[]} adminBlogs - An array of blog objects the current user is admin of
- */
-export const adminBlogs = userInfo?.blogs?.filter(blog => blog.admin) ?? [];
-
-/**
- * {string[]} adminBlogNames - An array of blog names the current user is admin of
- */
-export const adminBlogNames = adminBlogs.map(blog => blog.name);
